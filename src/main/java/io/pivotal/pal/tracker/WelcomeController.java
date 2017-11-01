cbf1d3937dd80340884cb0b468bb7da253a29cf6
Sameer Rastogi <sameer.rastogi@gmail.com>
2017-11-01 12:12:22 +0530

    submitCloudNativeDeveloperCloudFoundry

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,22 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+
+    private String str;
+
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}")String msg){
+        str = msg;
+    }
     @GetMapping("/")
     public String sayHello(){
-        return "hello";
+
+        return str;
     }
 }
